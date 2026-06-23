be05984ffcb9951776ec46523e72dbab176a07ab
Romain Lefranc <rlefranc@lefresnoy.net>
2026-06-23 11:59:49 +0200

    fix(config): set standars urls

--- src/config.dev.js
+++ src/config.dev.js
@@ -1,14 +1,11 @@
 const config = {
-  // templateBaseUrl: "/views/",
-  // useHtml5Mode: false,
-  api_media_url: "https://api.lefresnoy.net/media/",
-  rest_uri_v2: "https://api.lefresnoy.net/v2/",
-  // rest_uri: "https://api.lefresnoy.net/v1/",
+  api_media_url: "http://localhost:8000/media/",
+  rest_uri_v2: "http://localhost:8000/v2/",
   v3_graph: "http://127.0.0.1:8000/graphql",
-  media_service: "https://media.lefresnoy.net/",
-  // reset_password_uri: "https://api.lefresnoy.net/account/reset_password/",
-  // archive_rest_uri: "https://ame.lefresnoy.net/plugins/api_search/",
-  // archive_rest_key: "",
+  media_service: "http://localhost:7000/",
+  reset_password_uri: "http://localhost:8000/account/reset_password/",
+  archive_rest_uri: "https://archivehost/",
+  archive_rest_key: "",
 };
 
 export default config;
